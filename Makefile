356e5f60e1249d60c2882504948e08f2335e1626
Sean Farrell <sean.farrell@rioki.org>
2020-08-25 20:41:10 +0200

    Moves files into VS compatible structure.

--- Makefile
+++ Makefile
@@ -5,24 +5,23 @@ $(error config.mk not built. Run configure script.)
 endif
 
 CXX      ?= g++
-CXXFLAGS += -Iinclude -DVERSION=\"$(VERSION)\" $(XML2_CFLAGS)
+CXXFLAGS += -I. -DVERSION=\"$(VERSION)\" $(XML2_CFLAGS)
 LDFLAGS  += 
 
-headers    = $(wildcard include/*.h)
-lib_hdr    = $(wildcard src/*.h)
-lib_src    = $(wildcard src/*.cpp)
+lib_hdr    = $(wildcard libxmlmm/*.h)
+lib_src    = $(wildcard libxmlmm/*.cpp)
 lib_libs   = $(XML2_LIBS) 
-test_hdr   = $(wildcard test/*.h)
-test_src   = $(wildcard test/*.cpp)
-test_libs  = $(lib_libs) -lUnitTest++
+test_hdr   = $(wildcard libxmlmm-test/*.h)
+test_src   = $(wildcard libxmlmm-test/*.cpp)
+test_libs  = $(lib_libs)
 extra_dist = Makefile README.md $(wildcard docs/*.md)
-dist_files = $(headers) $(lib_hdr) $(lib_src) $(test_hdr) $(test_src) $(extra_dist)
+dist_files = $(lib_hdr) $(lib_src) $(test_hdr) $(test_src) $(extra_dist)
 
-ifeq ($(MSYSTEM), MINGW32)
+ifeq ($(OS),Windows_NT)
   EXEEXT    = .exe  
   LIBEXT    = .dll
 else
-  EXEEXT    =
+  EXEEXT    = .exe
   LIBEXT    = .so  
 endif
 
@@ -33,14 +32,14 @@ all: libxmlmm$(LIBEXT)
 libxmlmm$(LIBEXT): $(patsubst %.cpp, %.o, $(lib_src))
 	$(CXX) -shared -fPIC $(CXXFLAGS) $(LDFLAGS) $^ $(lib_libs) -Wl,--out-implib=$(patsubst %$(LIBEXT),%.a, $@) -o $@
 
-check: test-libxmlmm$(EXEEXT)	
-	./test-libxmlmm$(EXEEXT)
+check: libxmlmm-test$(EXEEXT)	
+	./libxmlmm-test$(EXEEXT)
 
-test-libxmlmm$(EXEEXT): libxmlmm$(LIBEXT) $(patsubst %.cpp, %.o, $(test_src))
+libxmlmm-test$(EXEEXT): libxmlmm$(LIBEXT) $(patsubst %.cpp, %.o, $(test_src))
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) $^ $(test_libs) -o $@
 
 clean: 
-	rm -f src/*.o src/*.d test/*.o test/*.d libxmlmm$(LIBEXT) test-libxmlmm$(EXEEXT)	
+	rm -f libxmlmm/*.o libxmlmm/*.d libxmlmm-test/*.o libxmlmm-test/*.d libxmlmm$(LIBEXT) libxmlmm-test$(EXEEXT)	
 
 dist:
 	mkdir libxmlmm-$(VERSION)
@@ -50,7 +49,7 @@ dist:
 
 install: libxmlmm$(LIBEXT)
 	mkdir -p $(prefix)/include/libxmlmm
-	cp $(headers) $(prefix)/include/libxmlmm
+	cp $(lib_hdr) $(prefix)/include/libxmlmm
 	mkdir -p $(prefix)/lib
 	cp libxmlmm.a $(prefix)/lib
 	mkdir -p $(prefix)/lib/pkgconfig
---
+++ libxmlmm-test/CDataTest.cpp
@@ -0,0 +1,85 @@
+//
+// Copyright (c) 2008-2020 Sean Farrell
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#include <string>
+#include <stdexcept>
+#include "rtest.h"
+
+#include <libxmlmm/Document.h>
+#include <libxmlmm/Element.h>
+#include <libxmlmm/CData.h>
+
+// NOTE: Nodes can not live w/o their Document.
+
+SUITE(CDataTest)
+{
+//------------------------------------------------------------------------------
+    TEST(cdata_get_content)
+    {
+        xml::Document doc;
+
+        std::string xml =
+            "<?xml version=\"1.0\"?>\n"
+            "<test><![CDATA[This is a test.]]></test>\n";
+
+        doc.read_from_string(xml);
+
+        xml::Element* element = doc.get_root_element();
+        CHECK(element != NULL);
+
+        std::vector<xml::Node*> children = element->get_children();
+        CHECK_EQUAL(1, children.size());
+        xml::CData* c_data = dynamic_cast<xml::CData*>(children.at(0));
+        CHECK(c_data != NULL);
+        CHECK_EQUAL("This is a test.", c_data->get_content());
+    }
+
+//------------------------------------------------------------------------------
+    TEST(cdata_element_get_text)
+    {
+        xml::Document doc;
+
+        std::string xml =
+            "<?xml version=\"1.0\"?>\n"
+            "<test><![CDATA[This is a test.]]></test>\n";
+
+        doc.read_from_string(xml);
+
+        xml::Element* element = doc.get_root_element();
+        CHECK(element != NULL);
+
+        CHECK_EQUAL("This is a test.", element->get_text());
+    }
+
+//------------------------------------------------------------------------------
+    TEST(cdata_behaves_like_text_in_xpath)
+    {
+        xml::Document doc;
+
+        std::string xml =
+            "<?xml version=\"1.0\"?>\n"
+            "<test><![CDATA[This is a test.]]></test>\n";
+
+        doc.read_from_string(xml);
+        std::string text = doc.query_string("/test/text()");
+        CHECK_EQUAL("This is a test.", text);
+    }
+}
